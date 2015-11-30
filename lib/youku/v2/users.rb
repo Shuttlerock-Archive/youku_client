5519c826d99f348cfe54d16601b95e5b3f207cf4
BenjaminSchaaf <ben.schaaf@gmail.com>
2015-12-01 03:40:16 +1100

    Completed non-authenticated portion of the users API

--- lib/youku/v2/users.rb
+++ lib/youku/v2/users.rb
@@ -34,6 +34,57 @@ module Youku
           user_name: user_name,
         }
       end
+
+      # Public: Get favourite API.
+      #
+      # Returns the instance of Youku::V2::Users::Friendship.
+      def friendship
+        Friendship.new(client)
+      end
+
+      class Friendship < Base
+        BASE_URI = "#{BASE_URI}/friendship"
+
+        # Public: Get the users following a given user.
+        #
+        # See: http://open.youku.com/docs?id=26
+        #
+        # user_id   - The String Youku user ID.
+        # user_name - The String Youku user name.
+        # page      - The Integer page number.
+        # count     - The Integer page size.
+        #
+        # Returns the instance of Youku::V2::Request.
+        def followings(user_id: nil, user_name: nil, page: 1, count: 20)
+          Youku::V2::Request.new "#{BASE_URI}/followings.json", {
+            client_id: client.client_id,
+            user_id:   user_id,
+            user_name: user_name,
+            page:      page,
+            count:     count,
+          }
+        end
+
+        # Public: Get the users a given user is following.
+        #
+        # See: http://open.youku.com/docs?id=27
+        #
+        # user_id   - The String Youku user ID.
+        # user_name - The String Youku user name.
+        # page      - The Integer page number.
+        # count     - The Integer page size.
+        #
+        # Returns the instance of Youku::V2::Request.
+        def followers(user_id: nil, user_name: nil, page: 1, count: 20)
+          Youku::V2::Request.new "#{BASE_URI}/followers.json", {
+            client_id: client.client_id,
+            user_id:   user_id,
+            user_name: user_name,
+            page:      page,
+            count:     count,
+          }
+        end
+      end
     end
   end
 end
--- spec/youku/v2/users_spec.rb
+++ spec/youku/v2/users_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Youku::V2::Users do
 
   let(:client) { Youku::Client.new(client_id: 'client-id') }
-  let(:users)  { Youku::V2::Users.new(client) }
 
   describe '#show_batch' do
     let(:url)   { 'https://openapi.youku.com/v2/users/show_batch.json' }
@@ -17,7 +16,7 @@ describe Youku::V2::Users do
       stub_request(:get, url).with(query: query).to_return(status: 200)
     end
 
-    subject { users.show_batch(user_names: 'jackie_chan') }
+    subject { client.users.show_batch(user_names: 'jackie_chan') }
 
     it_should_behave_like 'a base Youku API V2 requests'
   end
@@ -34,8 +33,55 @@ describe Youku::V2::Users do
       stub_request(:get, url).with(query: query).to_return(status: 200)
     end
 
-    subject { users.show(user_name: 'jackie_chan') }
+    subject { client.users.show(user_name: 'jackie_chan') }
 
     it_should_behave_like 'a base Youku API V2 requests'
+  end
+
+  describe '#friendship' do
+    subject { client.users.friendship }
+
+    it { expect(subject).to be_a Youku::V2::Users::Friendship }
+    it { expect(subject.client).to eq client }
+  end
+
+  describe '::Friendship' do
+    describe '#followings' do
+      let(:url)   { 'https://openapi.youku.com/v2/users/friendship/followings.json' }
+      let(:query) { {
+        client_id: client.client_id,
+        user_id:   '',
+        user_name: 'jackie_chan',
+        page:      1,
+        count:     20,
+      } }
+
+      before do
+        stub_request(:get, url).with(query: query).to_return(status: 200)
+      end
+
+      subject { client.users.friendship.followings(user_name: 'jackie_chan') }
+
+      it_should_behave_like 'a base Youku API V2 requests'
+    end
+
+    describe '#followers' do
+      let(:url)   { 'https://openapi.youku.com/v2/users/friendship/followers.json' }
+      let(:query) { {
+        client_id: client.client_id,
+        user_id:   '',
+        user_name: 'jackie_chan',
+        page:      1,
+        count:     20,
+      } }
+
+      before do
+        stub_request(:get, url).with(query: query).to_return(status: 200)
+      end
+
+      subject { client.users.friendship.followers(user_name: 'jackie_chan') }
+
+      it_should_behave_like 'a base Youku API V2 requests'
+    end
   end
 end
